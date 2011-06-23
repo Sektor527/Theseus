e3b91316028056bc6bf7f67ca8a83dc6352697c9
vanherle <w.vanherle@softkinetic.net>
2011-06-23 13:53:04 +0200

    Added unit test project

--- UnitTests/Properties/AssemblyInfo.cs
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -5,15 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Theseus")]
+[assembly: AssemblyTitle("UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Theseus")]
+[assembly: AssemblyProduct("UnitTests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("UnitTests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf7c2f83-1ed7-44b4-a954-2c96b3c4d361")]
+[assembly: Guid("52b890a3-6679-44b3-be05-4df2351bd548")]
 
 // Version information for an assembly consists of the following four values:
 //
