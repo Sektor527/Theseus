cf6d470119832ea19f5403aa41655d931cc69f18
vanherle <w.vanherle@softkinetic.net>
2011-06-23 16:39:40 +0200

    Create path between two cells

--- Theseus/Data/Cell.cs
+++ Theseus/Data/Cell.cs
@@ -1,10 +1,65 @@
-﻿namespace Theseus.Data
+﻿using System;
+
+namespace Theseus.Data
 {
 	class Cell
 	{
+		public enum Direction
+		{
+			North,
+			East,
+			South,
+			West
+		}
+
 		public Cell North { get; set; }
 		public Cell East { get; set; }
 		public Cell South { get; set; }
 		public Cell West { get; set; }
+
+		private void SetPath(Direction dir, Cell to)
+		{
+			switch (dir)
+			{
+				case Direction.North:
+					North = to;
+					break;
+				case Direction.East:
+					East = to;
+					break;
+				case Direction.South:
+					South = to;
+					break;
+				case Direction.West:
+					West = to;
+					break;
+			}
+		}
+
+		public static void CreatePath(Cell from, Direction fromdir, Cell to)
+		{
+			Direction todir;
+			switch (fromdir)
+			{
+				case Direction.North:
+					CreatePath(from, fromdir, to, Direction.South);
+					break;
+				case Direction.South:
+					CreatePath(from, fromdir, to, Direction.North);
+					break;
+				case Direction.East:
+					CreatePath(from, fromdir, to, Direction.West);
+					break;
+				case Direction.West:
+					CreatePath(from, fromdir, to, Direction.East);
+					break;
+			}
+		}
+
+		public static void CreatePath(Cell from, Direction fromdir, Cell to, Direction todir)
+		{
+			from.SetPath(fromdir, to);
+			to.SetPath(todir, from);
+		}
 	}
 }
--- UnitTests/Data/CellTest.cs
+++ UnitTests/Data/CellTest.cs
@@ -15,5 +15,105 @@ namespace UnitTests.Data
 			Assert.IsNull(cell.South);
 			Assert.IsNull(cell.West);
 		}
+
+		[Test]
+		public void CreatePathWithDefaultDirection()
+		{
+			// Left to right (East <-> West)
+			{
+				var from = new Cell();
+				var to = new Cell();
+
+				Cell.CreatePath(from, Cell.Direction.East, to);
+
+				Assert.AreSame(to, from.East);
+				Assert.AreSame(from, to.West);
+
+				Assert.IsNull(from.North);
+				Assert.IsNull(from.South);
+				Assert.IsNull(from.West);
+
+				Assert.IsNull(to.North);
+				Assert.IsNull(to.East);
+				Assert.IsNull(to.South);
+			}
+
+			// Right to left (West <-> East)
+			{
+				var from = new Cell();
+				var to = new Cell();
+
+				Cell.CreatePath(from, Cell.Direction.West, to);
+
+				Assert.AreSame(to, from.West);
+				Assert.AreSame(from, to.East);
+
+				Assert.IsNull(from.North);
+				Assert.IsNull(from.East);
+				Assert.IsNull(from.South);
+
+				Assert.IsNull(to.North);
+				Assert.IsNull(to.South);
+				Assert.IsNull(to.West);
+			}
+
+			// Top to bottom (South <-> North)
+			{
+				var from = new Cell();
+				var to = new Cell();
+
+				Cell.CreatePath(from, Cell.Direction.South, to);
+
+				Assert.AreSame(to, from.South);
+				Assert.AreSame(from, to.North);
+
+				Assert.IsNull(from.North);
+				Assert.IsNull(from.East);
+				Assert.IsNull(from.West);
+
+				Assert.IsNull(to.East);
+				Assert.IsNull(to.South);
+				Assert.IsNull(to.West);
+			}
+
+			// Bottom to top (North <-> South)
+			{
+				var from = new Cell();
+				var to = new Cell();
+
+				Cell.CreatePath(from, Cell.Direction.North, to);
+
+				Assert.AreSame(to, from.North);
+				Assert.AreSame(from, to.South);
+
+				Assert.IsNull(from.East);
+				Assert.IsNull(from.South);
+				Assert.IsNull(from.West);
+
+				Assert.IsNull(to.North);
+				Assert.IsNull(to.East);
+				Assert.IsNull(to.West);
+			}
+		}
+
+		[Test]
+		public void CreatePathWithCustomDirection()
+		{
+			var from = new Cell();
+			var to = new Cell();
+
+			Cell.CreatePath(from, Cell.Direction.East, to, Cell.Direction.North);
+
+			Assert.AreSame(to, from.East);
+			Assert.AreSame(from, to.North);
+
+			Assert.IsNull(from.North);
+			Assert.IsNull(from.South);
+			Assert.IsNull(from.West);
+
+			Assert.IsNull(to.East);
+			Assert.IsNull(to.South);
+			Assert.IsNull(to.West);
+		}
 	}
 }
