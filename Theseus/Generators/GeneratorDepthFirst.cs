b6083e1480f93fa3bb6a075039f8495554383377
vanherle <w.vanherle@softkinetic.net>
2011-08-17 17:22:39 +0200

    Created Depth First generator

---
+++ Theseus/Generators/GeneratorDepthFirst.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Theseus.Data;
+
+namespace Theseus.Generators
+{
+	static class GeneratorDepthFirst
+	{
+		private static readonly Random Rnd = new Random((int)DateTime.Now.Ticks);
+		private static List<Cell> _visited;
+
+		private static Maze _maze;
+
+		public static void Generate(Maze maze)
+		{
+			// Initialize
+			_maze = maze;
+			_visited = new List<Cell>(maze.Size.X * maze.Size.Y);
+
+			// Choose start cell
+			Cell exit = maze.Cell(Rnd.Next(maze.Size.X - 1), maze.Size.Y - 1);
+
+			// Recursively go through cells and link them up with one another
+			Visit(exit);
+		}
+
+		private static void Visit(Cell cell)
+		{
+			_visited.Add(cell);
+
+			// TODO: randomize order of Neighbors to make an interesting maze
+			foreach (Cell neighbor in cell.Neighbors)
+			{
+				if (neighbor == null) continue;
+				if (_visited.Contains(neighbor)) continue;
+
+				Cell.CreatePath(cell, neighbor);
+				Visit(neighbor);
+			}
+		}
+	}
+}
