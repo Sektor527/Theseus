a55cdeac22c7ca3740e57ac8aafe735087ef2f74
vanherle <w.vanherle@softkinetic.net>
2011-08-18 08:04:55 +0200

    Extracted function to plot

--- ConsoleViewer/Program.cs
+++ ConsoleViewer/Program.cs
@@ -13,61 +13,89 @@ namespace ConsoleViewer
 		{
 			Maze maze = new Maze(4, 6);
 
+			Generate(maze);
+			byte[][] plot = Plot(maze);
+			Draw(plot, maze.Size);
+
+			Console.ReadKey();
+		}
+
+		private static void Generate(Maze maze)
+		{
 			Cell start = maze.Cell(0, 0);
 			Cell.CreatePath(start, Cell.Direction.South);
 			Cell.CreatePath(maze.Cell(0, 1), maze.Cell(1, 1));
+			Cell.CreatePath(maze.Cell(1, 1), maze.Cell(1, 2));
+			Cell.CreatePath(maze.Cell(1, 1), maze.Cell(2, 1));
+			Cell.CreatePath(maze.Cell(0, 1), maze.Cell(0, 2));
+		}
 
-			for (int y = 0; y < maze.Size.Y * 2 + 1; ++y)
+		private static byte[][] Plot(Maze maze)
+		{
+			byte[][] plot = new byte[maze.Size.Y * 2 + 1][];
+			for (int y = 0; y < maze.Size.Y*2 + 1; ++y)
 			{
-				for (int x = 0; x < maze.Size.X * 2 + 1; ++x)
+				plot[y] = new byte[maze.Size.X * 2 + 1];
+				for (int x = 0; x < maze.Size.X*2 + 1; ++x)
 				{
-					if (y % 2 == 0) // Horizontal wall line
+					if (y%2 == 0) // Horizontal wall line
 					{
-						if (y == maze.Size.Y * 2) // Last horizontal line
+						if (y == maze.Size.Y*2) // Last horizontal line
 						{
-							Console.Write("*");
+							plot[y][x] = 1; //Console.Write("*");
 						}
-						else if (x % 2 == 0) // Vertical wall line
+						else if (x%2 == 0) // Vertical wall line
 						{
 							// Corner points are always filled
-							Console.Write("*");
+							plot[y][x] = 1; //Console.Write("*");
 						}
 						else // Vertical room line
 						{
 							// North-south passages
-							Cell cell = maze.Cell((x - 1) / 2, y / 2);
+							Cell cell = maze.Cell((x - 1)/2, y/2);
 							if (cell.NorthOpen)
-								Console.Write(" ");
+								plot[y][x] = 0; //Console.Write(" ");
 							else
-								Console.Write("*");
+								plot[y][x] = 1; //Console.Write("*");
 						}
 					}
 					else // Horizontal room line
 					{
-						if (x == maze.Size.X * 2) // Last vertical line
+						if (x == maze.Size.X*2) // Last vertical line
 						{
-							Console.Write("*");
+							plot[y][x] = 1; //Console.Write("*");
 						}
-						else if (x % 2 == 0) // Vertical wall line
+						else if (x%2 == 0) // Vertical wall line
 						{
 							// East-west passages
-							Cell cell = maze.Cell(x / 2, (y - 1) / 2);
+							Cell cell = maze.Cell(x/2, (y - 1)/2);
 							if (cell.WestOpen)
-								Console.Write(" ");
+								plot[y][x] = 0; //Console.Write("*");
 							else
-								Console.Write("*");
+								plot[y][x] = 1; //Console.Write("*");
 						}
 						else // Vertical room line
 						{
 							// Rooms are always open
-							Console.Write(" ");
+							plot[y][x] = 0; //Console.Write("*");
 						}
 					}
 				}
 
+				//Console.WriteLine();
+			}
+
+			return plot;
+		}
+
+		private static void Draw(byte[][] plot, Size size)
+		{
+			for (byte y = 0; y < plot.Length; ++y)
+			{
+				for (byte x = 0; x < plot[y].Length; ++x)
+					Console.Write(plot[y][x] == 0 ? " " : "*");
 				Console.WriteLine();
 			}
-			Console.ReadKey();
 		}
 	}
 }
