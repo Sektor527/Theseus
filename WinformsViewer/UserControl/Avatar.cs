830b8a7ff89773562c1dd179c6c712c4cfd54fef
Wim <wim.vanherle@telenet.be>
2011-09-26 21:27:09 +0200

    Added Avatar class

---
+++ WinformsViewer/UserControl/Avatar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Theseus.Data;
+
+namespace WinformsViewer.UserControl
+{
+	internal class Avatar
+	{
+		public Cell Cell { get; private set; }
+
+		public Avatar(Cell entrance)
+		{
+			Cell = entrance;
+		}
+
+		public void Move(Cell.Direction direction)
+		{
+			switch (direction)
+			{
+				case Cell.Direction.North:
+					if (Cell.NorthOpen) Cell = Cell.North;
+					break;
+				case Cell.Direction.East:
+					if (Cell.EastOpen) Cell = Cell.East;
+					break;
+				case Cell.Direction.South:
+					if (Cell.SouthOpen) Cell = Cell.South;
+					break;
+				case Cell.Direction.West:
+					if (Cell.WestOpen) Cell = Cell.West;
+					break;
+			}
+		}
+	}
+}
