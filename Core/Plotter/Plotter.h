24336a4ff14c91be6c2b781af5083d60257bad29
vanherle <wva@softkinetic.com>
2012-04-12 13:38:08 +0200

    Created Plotter class

---
+++ Core/Plotter/Plotter.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Maze;
+
+class Plotter
+{
+public:
+	enum Pixels
+	{
+		Space = 0,
+		Wall,
+		Entrance,
+		Exit,
+
+		Void = 255
+	};
+
+	static Plotter* plot(Maze* maze);
+
+	size_t getSizeWidth() const;
+	size_t getSizeHeight() const;
+	char getPixel(size_t x, size_t y) const;
+
+private:
+	size_t _width, _height;
+	char** _plot;
+};
---
+++ CoreTest/Plotter/PlotterTest.cpp
@@ -0,0 +1,99 @@
+#include "gtest/gtest.h"
+
+#include "Data/Maze.h"
+#include "Plotter/Plotter.h"
+
+TEST(Plotter,CorrectSizes)
+{
+	Maze* maze = new Maze(4,6);
+	Plotter* plot = Plotter::plot(maze);
+
+	ASSERT_EQ(9, plot->getSizeWidth());
+	ASSERT_EQ(13, plot->getSizeHeight());
+}
+
+TEST(Plotter,ContainsBasicElements)
+{
+	Maze* maze = new Maze(2,2);
+	Plotter* plot = Plotter::plot(maze);
+
+	size_t foundSpace = 0;
+	size_t foundWall = 0;
+	size_t foundEntrance = 0;
+	size_t foundExit = 0;
+	size_t foundOther = 0;
+
+	for (size_t h = 0; h < plot->getSizeHeight(); ++h)
+	{
+		for (size_t w = 0; w < plot->getSizeWidth(); ++w)
+		{
+			switch (plot->getPixel(w, h))
+			{
+			case Plotter::Space:
+				foundSpace++;
+				break;
+			case Plotter::Wall:
+				foundWall++;
+				break;
+			case Plotter::Entrance:
+				foundEntrance++;
+				break;
+			case Plotter::Exit:
+				foundExit++;
+				break;
+			default:
+				foundOther++;
+				break;
+			}
+		}
+	}
+
+	ASSERT_GT(foundSpace, 0);
+	ASSERT_GT(foundWall, 0);
+
+	ASSERT_EQ(foundEntrance, 1);
+	ASSERT_EQ(foundExit, 1);
+
+	ASSERT_EQ(foundOther, 0);
+}
+
+TEST(Plotter,MazeBorders)
+{
+	Maze* maze = new Maze(3, 3);
+	Plotter* plot = Plotter::plot(maze);
+
+	// Top border
+	for (size_t x = 0; x < plot->getSizeWidth(); ++x)
+	{
+		if (x == 1)
+			ASSERT_EQ(Plotter::Entrance, plot->getPixel(x, 0));
+		else
+			ASSERT_EQ(Plotter::Wall, plot->getPixel(x, 0));
+	}
+
+	// Bottom border
+	for (size_t x = 0; x < plot->getSizeWidth(); ++x)
+	{
+		if (x == plot->getSizeWidth() - 2)
+			ASSERT_EQ(Plotter::Exit, plot->getPixel(x, plot->getSizeHeight()-1));
+		else
+			ASSERT_EQ(Plotter::Wall, plot->getPixel(x, plot->getSizeHeight()-1));
+	}
+
+	// Left border
+	for (size_t y = 0; y < plot->getSizeHeight(); ++y)
+		ASSERT_EQ(Plotter::Wall, plot->getPixel(0, y));
+
+	// Right border
+	for (size_t y = 0; y < plot->getSizeHeight(); ++y)
+		ASSERT_EQ(Plotter::Wall, plot->getPixel(plot->getSizeWidth()-1, y));
+}
+
+TEST(Plotter,EntranceAndExitPlacement)
+{
+	Maze* maze = new Maze(3, 3);
+	Plotter* plot = Plotter::plot(maze);
+
+	ASSERT_EQ(Plotter::Entrance, plot->getPixel(1, 0));
+	ASSERT_EQ(Plotter::Exit, plot->getPixel(plot->getSizeWidth()-2,plot->getSizeHeight()-1));
+}
